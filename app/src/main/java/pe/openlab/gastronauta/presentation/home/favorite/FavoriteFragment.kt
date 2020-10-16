abc754150e1587004f293548125d3dd364a783a8
bryam.soto <bryam.soto@avantica.com>
2020-10-16 12:52:11 -0500

    Implement Discover UI Implement Favorite UI

--- app/src/main/java/pe/openlab/gastronauta/presentation/home/favorite/FavoriteFragment.kt
+++ app/src/main/java/pe/openlab/gastronauta/presentation/home/favorite/FavoriteFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -28,9 +29,8 @@ class FavoriteFragment : Fragment() {
         favoriteViewModel =
             ViewModelProvider(this).get(FavoriteViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_favorite, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
         favoriteViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
         })
         return root
     }
